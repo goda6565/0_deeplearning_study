8ade012739acddf3bab67716974f00bf7c06a5df
goda <godaharuto@gmail.com>
2024-07-29 00:08:54 +0900

    third commit

--- titanic_nn.py
+++ titanic_nn.py
@@ -9,7 +9,6 @@ gender_submission = pd.read_csv("dataset/gender_submission.csv")
 #特徴量エンジニアリング
 #train
 t_train = train["Survived"]
-print(t_train)
 train["FamilySize"] = train["SibSp"] + train["Parch"] 
 x_train = train[["Pclass","Sex","Age","FamilySize","Fare","Embarked"]]
 x_train["Sex"].replace(["male","female"],[0,1],inplace=True)
@@ -22,7 +21,6 @@ x_train.fillna(x_train.mean(), inplace=True)
 #正規化
 x_train = (x_train - x_train.mean()) / x_train.std()
 x_train
-print(x_train)
 #test
 test["FamilySize"] = test["SibSp"] + test["Parch"] 
 x_test = test[["Pclass","Sex","Age","FamilySize","Fare","Embarked"]]
@@ -36,5 +34,3 @@ x_test.fillna(x_test.mean(), inplace=True)
 #正規化
 x_test = (x_test - x_test.mean()) / x_test.std()
 x_test
-print(x_test)
-
