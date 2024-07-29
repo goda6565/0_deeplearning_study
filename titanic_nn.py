16316d366cace79063ba91c5a3ad20d1061838e0
goda_desk <godaharuto@gmail.com>
2024-07-29 23:26:18 +0900

    keras

--- titanic_nn.py
+++ titanic_nn.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.utils import to_categorical
+import warnings
+warnings.filterwarnings('ignore')
+
 #データ読み込み
 train = pd.read_csv("dataset/train.csv")
 test = pd.read_csv("dataset/test.csv")
@@ -34,3 +41,31 @@ x_test.fillna(x_test.mean(), inplace=True)
 #正規化
 x_test = (x_test - x_test.mean()) / x_test.std()
 x_test
+
+#NNを作成する
+Model = Sequential()
+Model.add(Dense(100, input_dim=8))
+Model.add(Activation("relu"))
+Model.add(Dropout(0.15))
+Model.add(Dense(100))
+Model.add(Activation("relu"))
+Model.add(Dropout(0.5))
+Model.add(Dense(100))
+Model.add(Activation("relu"))
+Model.add(Dropout(0.5))
+Model.add(Dense(100))
+Model.add(Activation("relu"))
+Model.add(Dropout(0.5))
+Model.add(Dense(2))
+Model.add(Activation("softmax"))
+Model.compile(optimizer = "adam", loss = "categorical_crossentropy")
+t_train = to_categorical(t_train, num_classes=2)
+Model.fit(x_train,t_train,epochs=50)
+t_test = Model.predict(x_test)
+t_test = np.argmax(t_test, axis = 1)
+x_test["Survived"] = t_test
+t_output = x_test["Survived"]
+t_output.index = t_output.index + 892
+t_output.index.rename('PassengerId', inplace=True)
+# DataFrameをCSVに出力
+t_output.to_csv("output.csv")
